513f28be8176c11daf6143180894bf88cec3d88e
Ronak <ronakk@itpathsolutions.com>
2023-09-06 19:50:24 +0530

    Vercel config file integrated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import { Router } from 'express'
 import authRouter from './auth/index.js'
+import { TYPES } from '../utils/constant.js'
 
 const router = Router()
+router.get('/', async(req, res) => res.json({
+    type: TYPES.SUCCESS,
+    message: 'Server started.'
+}))
 router.use(authRouter)
 
 
